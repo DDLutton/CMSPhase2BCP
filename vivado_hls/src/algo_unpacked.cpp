60f01b53505ebb43e8e8742b129e8c959cd148df
ccaillol <ccaillol@cern.ch>
2019-05-22 06:46:57 -0400

    Prasanna's fixes + uint24_t for coeff

--- vivado_hls/src/algo_unpacked.cpp
+++ vivado_hls/src/algo_unpacked.cpp
@@ -11,10 +11,11 @@
 using namespace std;
 
 //#include "algo_unpacked.h"   // This is where you should have had hls_algo - if not find the header file and fix this - please do not copy this file as that defines the interface
-#include "../../../../../APx_Gen0_Algo/VivadoHls/null_algo_unpacked/vivado_hls/src/algo_unpacked.h"
+#include "../src/algo_unpacked.h"
 #include "TPG.hh"
+#include "../data/LUT.h"
 
-const uint4_t NCrystalsPerLink = 11; // Bits 16-31, 32-47, ..., 176-191, keeping range(15, 0) unused
+const int NCrystalsPerLink = 11; // Bits 16-31, 32-47, ..., 176-191, keeping range(15, 0) unused
 
  /*
   * algo_unpacked interface exposes fully unpacked input and output link data.
@@ -37,35 +38,33 @@ void algo_unpacked(ap_uint<192> link_in[N_CH_IN], ap_uint<192> link_out[N_CH_OUT
 #pragma HLS PIPELINE II=3
 #pragma HLS INTERFACE ap_ctrl_hs port=return
    
-
-	// null algo specific pragma: avoid fully combinatorial algo by specifying min latency
-	// otherwise algorithm clock input (ap_clk) gets optimized away
+// null algo specific pragma: avoid fully combinatorial algo by specifying min latency
+// otherwise algorithm clock input (ap_clk) gets optimized away
 #pragma HLS latency min=3
 
-        for (int lnk = 0; lnk < N_CH_IN; lnk++) {
-#pragma HLS UNROLL
-
-           static registers reg[NCrystalsPerLink];
-//#pragma HLS PIPELINE II=5
+static registers reg[N_CH_IN][NCrystalsPerLink];
 #pragma HLS ARRAY_PARTITION variable=reg complete dim=0
 
-           short i;
-           for (i = 0; i < NCrystalsPerLink; i++){
+	for (int8_t lnk = 0; lnk < N_CH_IN; lnk++) {
 #pragma HLS UNROLL
-               int bitLo=(1+i)*16;
-	       int bitHi_in=bitLo+13; // digi inputs are 14 bits
-               int bitHi_out=bitLo+15; // crystal outputs are 16 bits
-	       uint24_t mycoeff=0xb7506a; // FIXME take the coefficient from LUTs
-               link_out[lnk].range(bitHi_out,bitLo)=TPG(link_in[lnk].range(bitHi_in,bitLo), mycoeff, reg[i], i);
-           }
-  	}
+
+		for (int8_t i = 0; i < NCrystalsPerLink; i++){
+#pragma HLS UNROLL
+			short bitLo = (1+i)*16;
+			short bitHi_in = bitLo+13; // digi inputs are 14 bits
+			short bitHi_out = bitLo+15; // crystal outputs are 16 bits
+			uint24_t mycoeff = coeff[lnk*NCrystalsPerLink+i];//0xb7506a;//coeff[lnk*NCrystalsPerLink+i]; // FIXME take the coefficient from LUTs
+			link_out[lnk].range(bitHi_out, bitLo) = TPG(link_in[lnk].range(bitHi_in, bitLo), mycoeff, reg[lnk][i]);
+			if (link_out[lnk].range(bitHi_out, bitLo)>0) cout<<int(lnk)<<" "<<int(i)<<" "<<link_out[lnk].range(bitHi_out, bitLo)<<endl;
+		}
+	}
 
 	// Comment the following not to overwrite the output
-	for (int lnk = 0; lnk < N_CH_IN; lnk++) {
+	/*for (int8_t lnk = 0; lnk < N_CH_IN; lnk++) {
 #pragma HLS UNROLL
 //  pass-through "algo"
         link_out[lnk].range(7,0) = 0;
         link_out[lnk].range(191,8) = link_in[lnk].range(191,8) ;
-    }
+    }*/
 }
 
