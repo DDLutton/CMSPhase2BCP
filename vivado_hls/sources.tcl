44dc80aeff9c789b64f9e9916b230c0bd553e1f6
Dylan Lutton <dylan.dale.lutton@cern.ch>
2019-07-16 01:35:16 -0400

    Setting the framework variables appropriately

--- vivado_hls/sources.tcl
+++ vivado_hls/sources.tcl
@@ -1,14 +1,14 @@
 ## Set the top level module
-set_top algo_unpacked_DCLRps_NojR_S
+set_top algo_unpacked
 ##
 #### Add source code
-add_files src/algo_unpacked_DCLRps_NojR_S.cpp
+add_files src/algo_unpacked.cpp
 add_files src/TPG.cc
 
 #
 ### Add testbed files
-add_files -tb src/algo_unpacked_DCLRps_NojR_S_tb.cpp 
+add_files -tb src/algo_unpacked_tb.cpp 
 
 ### Add test input files
-add_files -tb data/test2_DCLRps_NojR_S_inp.txt
-add_files -tb data/test2_DCLRps_NojR_S_out_ref.txt
+add_files -tb data/test2_inp.txt
+add_files -tb data/test2_out_ref.txt
